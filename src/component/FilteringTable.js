9fa103d3d00d143b98b75b7d81653ef28dd98a93
sksurendrakmr <sksurendrakmr@gmail.com>
2021-08-27 19:17:36 +0530

    created a component and renamed a component

--- src/component/FilteringTable.js
+++ src/component/FilteringTable.js
@@ -1,53 +1,4 @@
 /**
- Sorting-> How to implement sorting feature in react table
- 
- Note-> All feature in react table are implemented by importing one or the other hook.
- 
- step 1-> import sortBy hook from react table
- 
- step 2-> pass in as the second argument to the useTable hook.
- (it will adds the sorting feature to our table instance.
-    The sorting feature can be directly made use of in the jsx)
-
-step 3-> In the <th> tag, we will pass column.getSortByToggleProps() as an argument
-        in getHeaderProps() function.
-        This will adds properties related to the sorting feature on the
-        individual column.
-
-step 4-> After rendering column header label, we can now add an icons 
-        to represent the sorted state of the column
- */
-
-/**
- * Sorting and formatting
- * If we have a date in string format, sorting would happen as if date
- * coulmn is a string field and not date field.
- *
- * So we have to make sure that the data we provide to the table is not
- * already formatted.
- *
- * Note: - React table needs proper date format as its input to support
- * sorting
- *But react table provide a simple way to format any column data.
-
- In our case we want to format the date fields
- In COLUMNS array, the column which we want to format, define another
- property called Cell
- 
- The Cell property controls what is rendered in the UI.
- Cell property is equal to a function and it receives a couple of things
- as its argument but for this case we are only interested in the raw
- value of the column.
-
- When dealing with date column and sorting, make sure to preserve the date type
- when passing the data into the react table.
- For formatting use the Cell property for each column.
- We will get access to the raw value of each row for that column and we can
- transform that value into any desired format.
-
- */
-
-/**
  * Global Filtering
  * We have two types of filtering:-
  * 1) Global Filtering
@@ -65,7 +16,7 @@ import MOCK_DATA from './MOCK_DATA.json'
 import { COLUMNS, GROUPED_COLUMNS } from './columns'
 import './table.css'
 
-const SortingTable = () => {
+const FilteringTable = () => {
   const columns = useMemo(() => COLUMNS, [])
   // const columns = useMemo(() => GROUPED_COLUMNS, [])
   const data = useMemo(() => MOCK_DATA, [])
@@ -133,4 +84,4 @@ const SortingTable = () => {
   )
 }
 
-export default SortingTable
+export default FilteringTable
