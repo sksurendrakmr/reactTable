47ea90c27516262a95fc2456310a2893bcfdca88
sksurendrakmr <sksurendrakmr@gmail.com>
2021-08-27 19:38:35 +0530

    Implemented GlobalFiltering using reactTable

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import logo from './logo.svg'
 import './App.css'
 import BasicTable from './component/BasicTable'
 import SortingTable from './component/SortingTable'
+import FilteringTable from './component/GlobalFilter/FilteringTable'
 
 function App() {
   return (
     <div className='App'>
       <h1>React Table</h1>
       {/* <BasicTable /> */}
-      <SortingTable />
+      {/* <SortingTable /> */}
+      <FilteringTable />
     </div>
   )
 }
